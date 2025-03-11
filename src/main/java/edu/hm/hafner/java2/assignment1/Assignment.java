409c3e623481b37ac305eaa4c94e2465c6aa9e9d
Ulli Hafner <ullrich.hafner@gmail.com>
2025-03-11 10:18:02 +0100

    Add solution for assignment 1
    The assignment uses an array to store the result of the assignment test
    cases. The exam aggregates `n` assignments and stores these in an array.
     The grade is evaluated by comparing the success rate.

--- src/main/java/edu/hm/hafner/java2/assignment1/Assignment.java
+++ src/main/java/edu/hm/hafner/java2/assignment1/Assignment.java
@@ -4,4 +4,89 @@ package edu.hm.hafner.java2.assignment1;
  * Representation of an assignment.
  */
 public class Assignment {
+    private final int number;
+    private final boolean[] testCases;
+    private final int tests; // redundant as implicitly given by array length
+
+    /**
+     * Creates a new instance of {@link Assignment}.
+     *
+     * @param number
+     *         the assignment number
+     * @param tests
+     *         the number of test cases
+     */
+    public Assignment(final int number, final int tests) {
+        this.number = number;
+        this.testCases = new boolean[tests];
+        this.tests = tests;
+    }
+
+    public int getNumber() {
+        return number;
+    }
+
+    public int getTests() {
+        return tests;
+    }
+
+    /**
+     * Returns the number of green tests.
+     *
+     * @return the number of solved tests.
+     */
+    public int getGreen() {
+        int solvedAmount = 0;
+        for (boolean isTestGreen : testCases) {
+            if (isTestGreen) {
+                solvedAmount++;
+            }
+        }
+        return solvedAmount;
+    }
+
+    /**
+     * Get number of red tests.
+     *
+     * @return number of unsolved tests
+     */
+    public int getRed() {
+        return tests - getGreen();
+    }
+
+    /**
+     * Get percentage of solved tests.
+     *
+     * @return percentage of solved tests
+     */
+    public int getPercentage() {
+        int solvedAmount = getGreen();
+
+        return 100 * solvedAmount / getTests();
+    }
+
+    /**
+     * Solves a test with the given index.
+     *
+     * @param index
+     *         index of test to solve
+     * @throws IndexOutOfBoundsException if the index is out of bounds
+     */
+    public void solve(final int index)
+            throws IndexOutOfBoundsException {
+        if (index < 0 || index >= testCases.length) {
+            throw new IndexOutOfBoundsException(
+                    "Index out of bounds: " + index);
+        }
+        testCases[index] = true;
+    }
+
+    /**
+     * Returns whether the percentage of solved tests is greater or equal than 50 percent.
+     *
+     * @return if achieved percentage is enough
+     */
+    public boolean isSufficient() {
+        return getPercentage() >= 50;
+    }
 }
